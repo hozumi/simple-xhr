f6585254b767261d88244b5dd32081fa77d2bc1d
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2013-01-19 11:30:31 +0900

    remove clj->js because official version of it is provided now

--- src/simple_xhr.cljs
+++ src/simple_xhr.cljs
@@ -8,20 +8,6 @@
             [goog.json :as gjson]
             [goog.net.XhrManager :as manager]))
 
-;; http://mmcgrana.github.com/2011/09/clojurescript-nodejs.html
-(defn clj->js
-  "Recursively transforms ClojureScript maps into Javascript objects
-   other ClojureScript colls into JavaScript arrays, and ClojureScript
-   keywords into JavaScript strings."
-  [x]
-  (cond
-    (string? x) x
-    (keyword? x) (name x)
-    (map? x) (.-strobj (reduce (fn [m [k v]]
-               (assoc m (clj->js k) (clj->js v))) {} x))
-    (coll? x) (apply array (map clj->js x))
-    :else x))
-
 (def xhr-manager
   (atom (goog.net.XhrManager. js/undefined
                               js/undefined
